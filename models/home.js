5e716453dbcf6224e61868fbf1dafee0e063bb4c
harsh goyal <18harshgoyal@gmail.com>
2025-07-31 10:12:35 +0530

    addfiles

---
+++ models/home.js
@@ -0,0 +1,21 @@
+
+const mongoose = require('mongoose');
+
+
+const homeSchema = new mongoose.Schema({
+  houseName: { type: String, required: true },
+  price: { type: Number, required: true },
+  location: { type: String, required: true },
+  rating: { type: Number, required: true },
+  photo: String,
+  description: String,
+});
+
+
+homeSchema.pre('findOneAndDelete', async function(next) {
+  console.log('Came to pre hook while deleting a home');
+  const homeId = this.getQuery()._id;
+  await favourite.deleteMany({houseId: homeId});
+  next();
+});
+module.exports = mongoose.model('Home', homeSchema);
